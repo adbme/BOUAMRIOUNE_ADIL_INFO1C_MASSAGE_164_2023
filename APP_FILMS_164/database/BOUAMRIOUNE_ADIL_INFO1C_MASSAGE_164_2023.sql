5a9a998190b8f7ea7bd4723ed1746aa93f17c5cc
ad.bme <you@example.com>
2023-06-01 09:45:47 +0200

    mis a jour du dump, enfiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiin

--- APP_FILMS_164/database/BOUAMRIOUNE_ADIL_INFO1C_MASSAGE_164_2023.sql
+++ APP_FILMS_164/database/BOUAMRIOUNE_ADIL_INFO1C_MASSAGE_164_2023.sql
@@ -28,6 +28,11 @@ CREATE TABLE IF NOT EXISTS `mail` (
 
 -- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.mail : 4 rows
 /*!40000 ALTER TABLE `mail` DISABLE KEYS */;
+INSERT INTO `mail` (`id_mail`, `adresse_mail`) VALUES
+	(1, 'david@lescramptés'),
+	(2, 'doe.jone@example'),
+	(3, 'mike#example.com'),
+	(4, 'sarah@example.com');
 /*!40000 ALTER TABLE `mail` ENABLE KEYS */;
 
 -- Listage de la structure de table bouamrioune_adil_info1c_massage_164_2023. personne
@@ -40,10 +45,15 @@ CREATE TABLE IF NOT EXISTS `personne` (
   PRIMARY KEY (`id_personne`),
   KEY `fk_mail` (`fk_mail`),
   KEY `fk_tel` (`fk_tel`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.personne : 8 rows
+-- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.personne : 4 rows
 /*!40000 ALTER TABLE `personne` DISABLE KEYS */;
+INSERT INTO `personne` (`id_personne`, `nom_personne`, `prenom_personne`, `fk_mail`, `fk_tel`) VALUES
+	(1, 'david', 'lescramptés', 1, 1),
+	(2, 'doe', 'jone', 2, 2),
+	(3, 'mike', 'larue', 3, 3),
+	(4, 'sarah', 'hmara', 4, 4);
 /*!40000 ALTER TABLE `personne` ENABLE KEYS */;
 
 -- Listage de la structure de table bouamrioune_adil_info1c_massage_164_2023. prendre_rdv
@@ -52,13 +62,17 @@ CREATE TABLE IF NOT EXISTS `prendre_rdv` (
   `date_prendre_rdv` date NOT NULL,
   `heure_prendre_rdv` time NOT NULL,
   `fk_personne` int NOT NULL,
-  `duree_film` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_personne` (`fk_personne`)
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.prendre_rdv : 4 rows
 /*!40000 ALTER TABLE `prendre_rdv` DISABLE KEYS */;
+INSERT INTO `prendre_rdv` (`id`, `date_prendre_rdv`, `heure_prendre_rdv`, `fk_personne`) VALUES
+	(1, '2023-06-02', '10:00:00', 1),
+	(2, '2023-06-03', '14:30:00', 2),
+	(3, '2023-06-04', '16:45:00', 3),
+	(4, '2023-06-05', '12:02:00', 4);
 /*!40000 ALTER TABLE `prendre_rdv` ENABLE KEYS */;
 
 -- Listage de la structure de table bouamrioune_adil_info1c_massage_164_2023. tel
@@ -70,6 +84,11 @@ CREATE TABLE IF NOT EXISTS `tel` (
 
 -- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.tel : 4 rows
 /*!40000 ALTER TABLE `tel` DISABLE KEYS */;
+INSERT INTO `tel` (`id_tel`, `num_tel`) VALUES
+	(1, '0784440927219'),
+	(2, '0219021020020'),
+	(3, '0912838382911'),
+	(4, '8933208329083');
 /*!40000 ALTER TABLE `tel` ENABLE KEYS */;
 
 -- Listage de la structure de table bouamrioune_adil_info1c_massage_164_2023. t_film
@@ -84,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `t_film` (
   PRIMARY KEY (`id_film`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.t_film : ~13 rows (environ)
+-- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.t_film : ~0 rows (environ)
 
 -- Listage de la structure de table bouamrioune_adil_info1c_massage_164_2023. t_genre
 CREATE TABLE IF NOT EXISTS `t_genre` (
@@ -95,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `t_genre` (
   UNIQUE KEY `intitule_genre` (`intitule_genre`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.t_genre : ~18 rows (environ)
+-- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.t_genre : ~0 rows (environ)
 
 -- Listage de la structure de table bouamrioune_adil_info1c_massage_164_2023. t_genre_film
 CREATE TABLE IF NOT EXISTS `t_genre_film` (
@@ -110,7 +129,7 @@ CREATE TABLE IF NOT EXISTS `t_genre_film` (
   CONSTRAINT `t_genre_film_ibfk_2` FOREIGN KEY (`fk_film`) REFERENCES `t_film` (`id_film`)
 ) ENGINE=InnoDB AUTO_INCREMENT=158 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.t_genre_film : ~17 rows (environ)
+-- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.t_genre_film : ~0 rows (environ)
 
 -- Listage de la structure de table bouamrioune_adil_info1c_massage_164_2023. t_test
 CREATE TABLE IF NOT EXISTS `t_test` (
@@ -118,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `t_test` (
   `adil` varchar(50) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.t_test : 13 rows
+-- Listage des données de la table bouamrioune_adil_info1c_massage_164_2023.t_test : 0 rows
 /*!40000 ALTER TABLE `t_test` DISABLE KEYS */;
 /*!40000 ALTER TABLE `t_test` ENABLE KEYS */;
 
