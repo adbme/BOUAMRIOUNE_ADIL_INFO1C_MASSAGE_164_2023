0db61a0bfccdb7810cfc71a12bdde720a510c472
ad.bme <you@example.com>
2023-06-01 10:02:03 +0200

    mis a jour des field de film add

--- APP_FILMS_164/films/gestion_films_wtf_forms.py
+++ APP_FILMS_164/films/gestion_films_wtf_forms.py
@@ -18,14 +18,7 @@ class FormWTFAddFilm(FlaskForm):
         Dans le formulaire "genres_ajouter_wtf.html" on impose que le champ soit rempli.
         Définition d'un "bouton" submit avec un libellé personnalisé.
     """
-    nom_film_regexp = ""
-    nom_film_add_wtf = StringField("Nom du film ", validators=[Length(min=2, max=2000, message="min 2 max 20"),
-                                                               Regexp(nom_film_regexp,
-                                                                      message="Pas de chiffres, de caractères "
-                                                                              "spéciaux, "
-                                                                              "d'espace à double, de double "
-                                                                              "apostrophe, de double trait union")
-                                                               ])
+
     inputDate = DateField("date")
     inputTime = TimeField("hour")
     inputInt = IntegerField("fk")
